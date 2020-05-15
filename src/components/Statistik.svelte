5c6a2ac4a7cdc3e0db76dd150a9f052f04a986fd
maklumi <maklumi@yahoo.com>
2020-05-15 14:04:32 +0800

    mxl

--- src/components/Statistik.svelte
+++ src/components/Statistik.svelte
@@ -1,1 +1,24 @@
-<h1>Statistik covid</h1>
+<style>
+  .columns {
+    border: solid 1px black;
+  }
+</style>
+
+<div class="section">
+  <div class="container">
+    <div class="columns">
+      <h2 class="is-3 column">Bilangan kes:</h2>
+      <h2 class="is-3 column">Kematian:</h2>
+      <h2 class="is-3 column">Sembuh:</h2>
+      <h2 class="is-3 column">Tarikh :</h2>
+    </div>
+  </div>
+  <div class="container">
+    <div class="columns">
+      <h2 class="is-3 column">Diuji:</h2>
+      <h2 class="is-3 column">Dalam hospital:</h2>
+      <h2 class="is-3 column">Dalam ICU:</h2>
+      <h2 class="is-3 column">Ventilated :</h2>
+    </div>
+  </div>
+</div>
