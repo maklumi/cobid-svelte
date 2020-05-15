86c3907bf8d5e9dd887491b5032b2094f85f7fbb
kfc <maklumi@yahoo.com>
2020-05-15 18:13:05 +0800

    populate header with true data

--- src/components/Statistik.svelte
+++ src/components/Statistik.svelte
@@ -1,3 +1,14 @@
+<script>
+export let kes;
+export let mati;
+export let sembuh;
+export let ventilator;
+export let hospitalised;
+export let icu;
+export let diuji;
+export let updated;
+</script>
+
 <style>
   .columns {
     border: solid 1px black;
@@ -7,18 +18,18 @@
 <div class="section">
   <div class="container">
     <div class="columns">
-      <h2 class="is-3 column">Bilangan kes:</h2>
-      <h2 class="is-3 column">Kematian:</h2>
-      <h2 class="is-3 column">Sembuh:</h2>
-      <h2 class="is-3 column">Tarikh :</h2>
+      <h2 class="is-3 column">Bilangan kes:{kes}</h2>
+      <h2 class="is-3 column">Kematian:{mati}</h2>
+      <h2 class="is-3 column">Sembuh:{sembuh}</h2>
+      <h2 class="is-3 column">Tarikh :{updated}</h2>
     </div>
   </div>
   <div class="container">
     <div class="columns">
-      <h2 class="is-3 column">Diuji:</h2>
-      <h2 class="is-3 column">Dalam hospital:</h2>
-      <h2 class="is-3 column">Dalam ICU:</h2>
-      <h2 class="is-3 column">Ventilated :</h2>
+      <h2 class="is-3 column">Diuji:{diuji}</h2>
+      <h2 class="is-3 column">Dalam hospital:{hospitalised}</h2>
+      <h2 class="is-3 column">Dalam ICU:{icu}</h2>
+      <h2 class="is-3 column">Ventilated :{ventilator}</h2>
     </div>
   </div>
 </div>
